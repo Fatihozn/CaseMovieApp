3106e67864136250adb59f79faed520feab543b9
Fatih Özen <100231583+Fatihozn@users.noreply.github.com>
2025-05-21 12:39:35 +0300

    Initial Commit

---
+++ CaseMovieApp/CaseMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseMovieAppApp.swift
+//  CaseMovieApp
+//
+//  Created by Fatih Özen on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CaseMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
